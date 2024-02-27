837dd4065f2b9718cff1cc056a18f6291eabc4b9
Saad khan <161139592+saadkhankorii@users.noreply.github.com>
2024-02-27 20:17:53 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// let saad = "hello wold!"
+// console.log(saad);
+// let name1 = "saad khan"
+// console.log(name1)
+// name1 = "saad khankori"
+// console.log(name1)
+// let userName:string = "khan"
+// userName = "Khankori"
+// userName = "10"
+// console.log(userName)
+var name1 = "saad";
+var age = 19;
+var isMarried = false;
+console.log(name1, age, isMarried);
+// let abc: undefined = undefined
+// let xcv: null = null
---
+++ main.ts
@@ -0,0 +1,18 @@
+// let saad = "hello wold!"
+// console.log(saad);
+
+// let name1 = "saad khan"
+// console.log(name1)
+// name1 = "saad khankori"
+// console.log(name1)
+
+
+let userName:string = "khan"
+userName = "Khankori"
+userName = "10"
+console.log(userName)
+
+// let name1: string = "saad"
+// let age:number = 19
+// let isMarried: boolean = false
+
